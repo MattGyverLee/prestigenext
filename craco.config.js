c5a2fc67c186435eb71e2bdd22dd424ca07a4cfc
Matthew Lee <matthew_lee@sil.org>
2023-02-24 01:03:11 +0100

    Working with delay

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,47 @@
+const fs = require("fs");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const evalSourceMap = require("react-dev-utils/evalSourceMapMiddleware");
+const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
+const noopServiceWorker = require("react-dev-utils/noopServiceWorkerMiddleware");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 module.exports = {
-  typescript: {
-    enableTypeChecking: true /* (default value) */,
+  /* typescript: {
+    enableTypeChecking: true 
+  }, */
+/*   configure: {
+    target: "electron-renderer"
+  }, */
+  webpack: {
+    plugins: {
+      add: [
+        new NodePolyfillPlugin({
+          excludeAliases: ["console"],
+        }),
+      ],
+    },
+  },
+  // https://github.com/facebook/create-react-app/issues/11860#issuecomment-1140417343
+
+
+  devServer: (devServerConfig, { env, paths }) => {
+    devServerConfig = {
+      onBeforeSetupMiddleware: undefined,
+      onAfterSetupMiddleware: undefined,
+      setupMiddlewares: (middlewares, devServer) => {
+        if (!devServer) {
+          throw new Error("webpack-dev-server is not defined");
+        }
+        if (fs.existsSync(paths.proxySetup)) {
+          require(paths.proxySetup)(devServer.app);
+        }
+        middlewares.push(
+          evalSourceMap(devServer),
+          redirectServedPath(paths.publicUrlOrPath),
+          noopServiceWorker(paths.publicUrlOrPath)
+        );
+        return middlewares;
+      },
+    };
+    return devServerConfig;
   },
 };
